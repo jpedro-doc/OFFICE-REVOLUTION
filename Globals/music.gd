781e8472871558fba325fb43ba97513f864ff9ca
SoaresDavidson <davi504r35@gmail.com>
2025-03-21 11:53:29 -0300

    score e cores nas setas

--- Globals/music.gd
+++ Globals/music.gd
@@ -3,3 +3,4 @@ extends Node2D
 @onready var main_menu_music: AudioStreamPlayer2D = $MainMenuMusic
 @onready var janitor_song: AudioStreamPlayer2D = $JanitorSong
 @onready var secretary_song: AudioStreamPlayer2D = $SecretarySong
+@onready var boss_song: AudioStreamPlayer2D = $BossSong
--- Scripts/Game/Arrow/blank_arrows.gd
+++ Scripts/Game/Arrow/blank_arrows.gd
@@ -5,9 +5,12 @@ extends Node2D
 @onready var up: Sprite2D = $up
 @onready var right: Sprite2D = $right
 
-var score = 0
+var score:int = 0: set = _set_score_label
+var hit_score:int = 50
+var miss_score:int = 25
+var rainbow_mult:int = 5
 
-var arrow_pressed_coords = 2
+var arrow_pressed_coords:int = 2
 
 func _ready() -> void:
 	pass
@@ -23,30 +26,34 @@ func _process(delta: float) -> void:
 		if $left/Area2D.has_overlapping_areas():
 			$left/Area2D.get_overlapping_areas().filter(get_rid)
 		else:
-			score -= 1
+			score -= miss_score
 	
 	if Input.is_action_just_pressed("down"):
 		down.frame_coords.y = arrow_pressed_coords
 		if $down/Area2D.has_overlapping_areas():
 			$down/Area2D.get_overlapping_areas().filter(get_rid)
 		else:
-			score -= 1
+			score -= miss_score
 	
 	if Input.is_action_just_pressed("up"):
 		up.frame_coords.y = arrow_pressed_coords
 		if $up/Area2D.has_overlapping_areas():
 			$up/Area2D.get_overlapping_areas().filter(get_rid)
 		else:
-			score -= 1
+			score -= miss_score
 		
 	if Input.is_action_just_pressed("right"):
 		right.frame_coords.y = arrow_pressed_coords
 		if $right/Area2D.has_overlapping_areas():
 			$right/Area2D.get_overlapping_areas().filter(get_rid)
 		else:
-			score -= 1
+			score -= miss_score
 		
 
 func get_rid(object:Node2D):
+	score += hit_score
 	object.get_parent().queue_free()
 	
+func _set_score_label(new_value:int):
+	score = new_value
+	$"../Label".text = "score: " + str(new_value)
