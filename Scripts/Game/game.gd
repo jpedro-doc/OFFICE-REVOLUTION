a491f476b6ea0a550f06a50fddaee68c9f3d686f
SoaresDavidson <davi504r35@gmail.com>
2025-03-21 08:13:26 -0300

    conserto de uns merge

--- Scripts/Game/game.gd
+++ Scripts/Game/game.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var timer: Timer = $Timer
 @export var difference: float
 
-var file = "res://notes_chart.json"
+@export var file = "res://notes_chart.json"
 var json_as_text = FileAccess.get_file_as_string(file)
 var json_as_dict = JSON.parse_string(json_as_text)
 
